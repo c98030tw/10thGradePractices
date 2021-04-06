f3288310fcd9a3317321456344a4c890f569f278
c98030tw <82030840+c98030tw@users.noreply.github.com>
2021-04-06 18:56:35 +0800

    Add files via upload

---
+++ firstcode.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	std::cout<<"        A        \n";
+	std::cout<<"      A A A      \n";
+	std::cout<<"    A A A A A    \n";
+	std::cout<<"      A A A      \n";
+	std::cout<<"    A A A A A    \n";
+	std::cout<<"  A A A A A A A  \n";
+	std::cout<<"    A A A A A    \n";
+	std::cout<<"  A A A A A A A  \n";
+	std::cout<<"A A A A A A A A A\n";
+	std::cout<<"      IIIII      \n";
+	std::cout<<"      IIIII      \n";
+	return 0;
+}
